2c6e691e011f40680701dceb61250984c6386c8f
muradmolla <muradmollaoglu@gmail.com>
2023-02-28 17:47:30 +0300

    Python websocket server initialized

--- server/main.py
+++ server/main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import asyncio
+import websockets
+import serial
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ser = serial.Serial("COM3", 9600)
 
+async def receive_data(websocket, path):
+    while True:
+        # Read data from the Arduino
+        data = read_data_from_arduino()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        # Send the data to the WebSocket client
+        await websocket.send(data)
 
+# Define a function to read data from the Arduino
+def read_data_from_arduino():
+    # Implement your code to read data from the Arduino here
+    line = ser.readline()
+    return line
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Start the WebSocket server
+start_server = websockets.serve(receive_data, "localhost", 8765)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
